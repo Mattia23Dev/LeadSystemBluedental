65ab20fa8b09600bc60cef53d54535beedc0ad78
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2024-06-21 12:00:15 +0200

    esclusa alessia

--- server/controllers/subs.js
+++ server/controllers/subs.js
@@ -80,7 +80,7 @@ axios.post(url, data, { headers })
 
 const calculateAndAssignLeadsEveryDay = async () => {
   try {
-    const excludedOrientatoreIds = ['660fc6b59408391f561edc1a', "65ddbe8676b468245d701bc2", "65ddbeaa76b468245d701bc5" /*ANGELICA*/, "6613a1389408391f56215308" /*STEFANIA*/];
+    const excludedOrientatoreIds = ['660fc6b59408391f561edc1a', "65ddbe8676b468245d701bc2", "65ddbeaa76b468245d701bc5" /*ANGELICA*/, "6613a1389408391f56215308" /*STEFANIA*/, "6613a0bd9408391f562152f5" /*ALESSIA*/];
 
     let users = await Orientatore.find({ _id: { $nin: excludedOrientatoreIds }, utente: "65d3110eccfb1c0ce51f7492"});
     let leads = await LeadFacebook.find({ 
@@ -354,7 +354,7 @@ const calculateAndAssignLeadsEveryDayMetaWeb = async () => {
     const callCenter = "664c5b2f3055d6de1fcaa22b";
     const callCenterUser = await User.findById(callCenter)
     const bludental = "65d3110eccfb1c0ce51f7492";
-    const excludedOrientatoreIds = ['660fc6b59408391f561edc1a', "65ddbe8676b468245d701bc2", "65ddbeaa76b468245d701bc5" /*ANGELICA*/, "6613a1389408391f56215308" /*STEFANIA*/];
+    const excludedOrientatoreIds = ['660fc6b59408391f561edc1a', "65ddbe8676b468245d701bc2", "65ddbeaa76b468245d701bc5" /*ANGELICA*/, "6613a1389408391f56215308" /*STEFANIA*/, "6613a0bd9408391f562152f5" /*ALESSIA*/];
 
     let users = await Orientatore.find({ _id: { $nin: excludedOrientatoreIds }});
     let leads = await LeadFacebook.find({
@@ -891,8 +891,8 @@ async function updateLeads() {
 }
 
 async function updateLeadsRec() {
-  const startDate = new Date('2024-05-15T00:00:00.000Z');
-  const endDate = new Date('2024-05-31T23:59:59.999Z');
+  const startDate = new Date('2024-06-01T00:00:00.000Z');
+  const endDate = new Date('2024-06-14T23:59:59.999Z');
   try {
     const excludedOrientatoreId = '660fc6b59408391f561edc1a';
       const leadsToUpdate = await Lead.find({ esito: "Non risponde", utmCampaign: /Meta Web/i });

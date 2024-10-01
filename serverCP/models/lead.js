aa21b49ab65ebdfa2cbdb2c20777d138a01c7c48
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2024-10-01 12:09:09 +0200

    ook

--- serverCP/models/lead.js
+++ serverCP/models/lead.js
@@ -6,6 +6,7 @@ const LeadSchema = new Schema({
       type: String,
       required: true
     },
+    dataTimestamp: { type: Date },
     lastModify: {
       type: String,
     },
@@ -91,6 +92,16 @@ const LeadSchema = new Schema({
     summary: String,
     appDate: String,
   });
+
+  LeadSchema.pre('save', function(next) {
+    if (this.data && !this.dataTimestamp) {
+      const dataDate = new Date(this.data);
+      if (!isNaN(dataDate)) {
+        this.dataTimestamp = dataDate;
+      }
+    }
+    next();
+  });  
 
 
 const Lead = mongoose.model('Lead', LeadSchema);

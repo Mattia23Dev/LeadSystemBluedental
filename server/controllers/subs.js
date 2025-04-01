9aa25484b5b58fc7f119514a391372afd2f4bf8c
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2025-04-01 11:15:57 +0200

    ok

--- server/controllers/subs.js
+++ server/controllers/subs.js
@@ -1076,22 +1076,22 @@ cron.schedule('30 4 * * *', () => {
   console.log('Eseguito il reset del daily Lead');
 });
 
-cron.schedule('10,46,20 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
+cron.schedule('10,46,20 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
   getDentistaLead();
   console.log('Prendo i lead di Bluedental 1.0');
 });
 
-cron.schedule('20,56,30 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
+cron.schedule('20,56,30 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
   getDentistaLead2();
   console.log('Prendo i lead di Bluedental 2.0');
 });
 
-cron.schedule('5,36,15 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
+cron.schedule('5,36,15 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
   getDentistaLead3();
   console.log('Prendo i lead di Bluedental 3.0');
 });
 
-cron.schedule('8,49,18 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
+cron.schedule('8,49,18 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
   getBludentalLead();
   console.log('Prendo i lead di Bluedental');
 });
@@ -1134,12 +1134,12 @@ async function updateDataTimestampForAllLeads() {
 
 //updateDataTimestampForAllLeads()
 
-cron.schedule('15,58,25,40 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
+cron.schedule('15,58,25,40 6,78,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
   calculateAndAssignLeadsEveryDay();
   console.log('Assegno i lead di bludental altro');
 });
 
-cron.schedule('20,10,35,50 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
+cron.schedule('20,10,35,50 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23 * * *', () => {
   calculateAndAssignLeadsEveryDayMetaWeb();
   console.log('Assegno i lead di bludental Meta web');
 });

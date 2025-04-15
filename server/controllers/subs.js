a997287b0677608ec65947d22296fe3fd81a9833
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2025-04-15 15:41:54 +0200

    ripartiamo con bludental DA

--- server/controllers/subs.js
+++ server/controllers/subs.js
@@ -870,13 +870,11 @@ const calculateAndAssignLeadsEveryDayMetaWeb = async () => {
 
             //await sendEmailLeadArrivati(user._id);
             console.log(currentHour)
-            /*if (lastFunctionExecuted !== 'calculateAndAssignLeadsEveryDayMetaWeb' && currentHour >= 7 && currentHour <= 19) {
               console.log("Eseguo la chiamata di Meta web per il lead " + newLead.email);
               await makeOutboundCall(newLead.numeroTelefono, newLead.città, newLead.nome, 'bludental');
               newLead.chiamato = true;
               await newLead.save();
-              lastFunctionExecuted = 'calculateAndAssignLeadsEveryDayMetaWeb';
-            }*/
+              //lastFunctionExecuted = 'calculateAndAssignLeadsEveryDayMetaWeb';
 
             console.log(`Assegnato il lead ${leadWithoutUser?._id} all'utente ${user.nome}`);            
           } else {

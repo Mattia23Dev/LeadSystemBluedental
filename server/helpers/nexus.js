a9636f371bc20c333aca0e9ff3693c5b520f1365
Mattia Noris <100127022+Mattia23Dev@users.noreply.github.com>
2025-11-26 17:28:58 +0100

    nexus update token

--- server/helpers/nexus.js
+++ server/helpers/nexus.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+//refresh 8O3sHfo4yI2H6YD0YW4H0cG7oOlNM7k1ng2aI6UEMiVby8rsHDYGvpTpz5JcuNQ2
 const nexus = axios.create({
   //baseURL: "https://test-bludental.hisolution.it",
   baseURL: "https://bludental.hisolution.it",
   headers: {
-    "Authorization": "Bearer 0P4EhBbxjBAuLTXMpKxPosRqgKuT7bC8KpTfmJtbfyEQ7ZymoA0qXmNEA4EkFSsH",
+    "Authorization": "Bearer VOXJZAbRz13TJU0YzOZRLv1A94RgQabxdougnKudA2RnCo2SqT8ci0cLQoUiSxoQ",
     "Content-Type": "application/json",
   },
 });
